9bbeae90baeab088ef4709f8e5e214d48846af45
Mathias Arzberger <develop@pdir.de>
2022-07-14 15:13:42 +0200

    Update PdirContaoWebtoolsBundleTest.php

--- tests/PdirContaoWebtoolsBundleTest.php
+++ tests/PdirContaoWebtoolsBundleTest.php
@@ -3,11 +3,18 @@
 declare(strict_types=1);
 
 /*
- * This file is part of [package name].
+ * Web-Tools Bundle for Contao Open Source CMS
  *
- * (c) John Doe
+ * Copyright (c) 2022 pdir / digital agentur // pdir GmbH
  *
- * @license LGPL-3.0-or-later
+ * @package    webtools-bundle
+ * @link       https://pdir.de/contao-webtools/
+ * @license    LGPL-3.0-or-later
+ * @author     Mathias Arzberger <develop@pdir.de>
+ * @author     Christian Mette <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Pdir\ContaoWebtools\Tests;
